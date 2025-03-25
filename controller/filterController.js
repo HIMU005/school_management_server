a8c507e096267a25393b5185de89c6a1533a8530
HIMU005 <hashanuzzaman99@gmail.com>
2025-03-25 16:08:54 +0600

    filter the student is done

--- controller/filterController.js
+++ controller/filterController.js
@@ -1,9 +1,24 @@
+import prisma from "../DB/db.config.js";
+
 export const filter_user_for_attendance = async (req, res) => {
   const { selectedClass } = req.query;
-  console.log(selectedClass);
+  try {
+    const students = await prisma.student.findMany({
+      where: {
+        class_id: Number(selectedClass),
+      },
+    });
 
-  return res.json({
-    status: 200,
-    message: `Filter user for attendance for class ${selectedClass}`,
-  });
+    return res.json({
+      status: 200,
+      message: "Students for attendance",
+      data: students,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      status: 500,
+      message: "Internal Server Error",
+      error: error.message,
+    });
+  }
 };
