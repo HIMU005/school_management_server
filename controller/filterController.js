0b1270ffc3b3dde0f24a2b0fec8d3fb8313ba89f
HIMU005 <hashanuzzaman99@gmail.com>
2025-06-29 10:58:49 +0600

    filter attendance for student is done

--- controller/filterController.js
+++ controller/filterController.js
@@ -23,6 +23,7 @@ export const filter_user_for_attendance = async (req, res) => {
   }
 };
 
+// filter for teacher
 export const show_attendance_after_filter = async (req, res) => {
   const selectedClass = parseInt(req.query.selectedClass);
 
@@ -45,6 +46,51 @@ export const show_attendance_after_filter = async (req, res) => {
     return res.json({
       status: 200,
       message: "all Students attendance",
+      data: attendances,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      status: 500,
+      message: "Internal Server Error",
+      error: error.message,
+    });
+  }
+};
+
+// filter for student
+export const show_filtered_attendance_by_date = async (req, res) => {
+  const { startDate, endDate, email } = req.query;
+  try {
+    const attendances = await prisma.attendance.findMany({
+      where: {
+        AND: [
+          {
+            student: {
+              user: {
+                email: email,
+              },
+            },
+          },
+          {
+            takingTime: {
+              gte: new Date(startDate),
+              lte: new Date(endDate),
+            },
+          },
+        ],
+      },
+      include: {
+        student: {
+          include: {
+            user: true,
+          },
+        },
+        class: true,
+      },
+    });
+    return res.json({
+      status: 200,
+      message: "all attendance of that range",
       data: attendances,
     });
   } catch (error) {
--- routes/filterRouter.js
+++ routes/filterRouter.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import {
   filter_user_for_attendance,
   show_attendance_after_filter,
+  show_filtered_attendance_by_date,
 } from "../controller/filterController.js";
 const router = Router();
 
@@ -10,5 +11,8 @@ router.get("/student_for_attendance", filter_user_for_attendance);
 
 // filter all student attendance by query provide
 router.get("/see_attendance", show_attendance_after_filter);
+
+// filter all data of a single student by date
+router.get("/see_attendance_by_date", show_filtered_attendance_by_date);
 
 export default router;
