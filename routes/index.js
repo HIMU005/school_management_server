016f9787804e56d2f16f3d33f3091b5350eb4930
HIMU005 <hashanuzzaman99@gmail.com>
2025-06-29 21:00:54 +0600

    install ssl commerz and create payment router

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,16 @@
 import { Router } from "express";
 import classRouter from "./classRouter.js";
 import filterRouter from "./filterRouter.js";
+import paymentRouter from "./paymentRouter.js";
 import studentRouter from "./studentRouter.js";
 import teacherRouter from "./teacherRouter.js";
 import updateRouter from "./updateRouter.js";
 import userRouter from "./userRouter.js";
+// import SSLCommerzPayment from "sslcommerz-lts";
+
+const store_id = process.env.STORE_ID;
+const store_passwd = process.env.STORE_PASS;
+const is_live = false;
 const router = Router();
 
 // route for user
@@ -24,5 +30,8 @@ router.use("/api/update_user", updateRouter);
 
 // filter router
 router.use("/api/filter", filterRouter);
+
+//payment router
+router.use("/api/payment", paymentRouter);
 
 export default router;
