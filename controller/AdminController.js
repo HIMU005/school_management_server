40f7cea79b5833247e8a6cd743e6bdd9449a1d14
HIMU005 <hashanuzzaman99@gmail.com>
2025-07-01 20:39:29 +0600

    subject add route added successfully

--- controller/AdminController.js
+++ controller/AdminController.js
@@ -1,3 +1,21 @@
+import prisma from "../DB/db.config.js";
+
 export const addBook = async (req, res) => {
-  console.log("hitted");
+  const bookData = req.body;
+  try {
+    const newBook = await prisma.subject.create({
+      data: bookData,
+    });
+    return res.json({
+      status: 201,
+      data: newBook,
+      message: "New book data uploaded successfully",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      status: 500,
+      message: "Internal Server Error",
+      error: error.message,
+    });
+  }
 };
